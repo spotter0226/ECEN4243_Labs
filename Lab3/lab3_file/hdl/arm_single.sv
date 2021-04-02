79ba3d58e81c785d6ba409cd477aa55890211ed4
spotter0226 <spotter0226@gmail.com>
2021-04-02 17:22:05 -0500

    Updated lab3 for implementing in lab 4

--- Lab3/lab3_file/hdl/arm_single.sv
+++ Lab3/lab3_file/hdl/arm_single.sv
@@ -427,10 +427,14 @@ module extend (input  logic [23:0] Instr,
                input  logic [ 1:0] ImmSrc,
                output logic [31:0] ExtImm);
    
-   always_comb
+  imm8 = {24b'0, Instr[7:0]}; 
+  rotate = Intsr[11:8] << 1;
+
+  newrotate = 
+  always_comb
      case(ImmSrc) 
        // 8-bit unsigned immediate
-       2'b00:   ExtImm = {24'b0, Instr[7:0]};  
+       2'b00:   ExtImm = rotate;  
        // 12-bit unsigned immediate 
        2'b01:   ExtImm = {20'b0, Instr[11:0]}; 
        // 24-bit two's complement shifted branch 
